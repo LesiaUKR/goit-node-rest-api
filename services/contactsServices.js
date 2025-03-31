8b9ebe187c96206011b234025e58e51f974e5e93
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-31 17:57:40 +0100

    fix(schema, contactsControllers): bugs fix

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -33,9 +33,9 @@ export async function addContact(name, email, phone) {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
-    name,
-    email,
-    phone,
+    ...name,
+    ...email,
+    ...phone,
   };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
